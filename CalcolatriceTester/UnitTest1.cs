fc53d3b5c899aad70833e51374f02bf04bee67de
diegoG82 <diegogiardina@hotmail.it>
2023-09-25 16:01:30 +0200

    Inseriti test sui metodi della calcolatrice

--- CalcolatriceTester/UnitTest1.cs
+++ CalcolatriceTester/UnitTest1.cs
@@ -1,3 +1,6 @@
+using net_calculator_tester;
+using NUnit.Framework.Internal;
+
 namespace CalcolatriceTester
 {
     public class Tests
@@ -5,12 +8,37 @@ namespace CalcolatriceTester
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("Test eseguito!");
         }
 
         [Test]
-        public void Test1()
+        public void Test_somma_con_risultato()
         {
-            Assert.Pass();
+            double risultato = Calcolatrice.Somma(5, 5);
+            Assert.AreEqual(10, risultato);
         }
+
+        [Test]
+        public void Test_sottrazione_con_risultato()
+        {
+            double risultato = Calcolatrice.Sottrazione(5, 5);
+            Assert.AreEqual(0, risultato);
+        }
+
+        [Test]
+        public void Test_moltiplicazione_con_risultato()
+        {
+            double risultato = Calcolatrice.Moltiplicazione(5, 5);
+            Assert.AreEqual(25, risultato);
+        }
+
+        [Test]
+        public void Test_divisione_con_risultato()
+        {
+            double risultato = Calcolatrice.Divisione(5, 5);
+            Assert.AreEqual(1, risultato);
+        }
+
+
     }
 }
